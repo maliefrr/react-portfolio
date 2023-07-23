805da65eb53f4bc61e479f37256ad97410ee4652
Mohamad Alief Rizky Ramadhan <aliefm144@gmail.com>
2023-07-23 13:58:51 +0800

    add image and typed.js on main-content

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,51 @@
+import { useEffect, useRef } from "react";
+import Typed from "typed.js"
 import Navbar from "./components/Navbar";
+import "./css/style.css";
+
+// Define the interface for the Typed.js options
+interface TypedOptions {
+  strings: string[];
+  typeSpeed: number;
+  backSpeed: number;
+  loop: boolean;
+  // Add other options here if needed
+}
 
 const App = () => {
+  const typedRef = useRef(null);
+
+  useEffect(() => {
+    const options: TypedOptions = {
+      strings: ["Halo, Namaku Alief!", "Seorang Fresh Graduate", "Love to Making Code", "Fullstack Web Developer", "React Native Enthusiast", "Welcome to my Portfolio Website!"],
+      typeSpeed: 50,
+      backSpeed: 30,
+      loop: true,
+    };
+
+    // Create a new instance of Typed.js
+    const typed = new Typed(typedRef.current, options);
+
+    // Clean up the Typed.js instance on unmount
+    return () => {
+      typed.destroy();
+    };
+  }, []);
 
   return (
     <>
       <Navbar />
+      <div className="main-content">
+        <div className="diamond-image">
+          <div className="diamond-content">
+            <img src="/image/profile_picture.jpeg" alt="Profile Picture" />
+          </div>
+        </div>
+        {/* Typed.js Output */}
+        <div className="typed-output">
+          <h1 ref={typedRef}></h1>
+        </div>
+      </div>
     </>
   );
 };
---
+++ src/typed-js.d.ts
@@ -0,0 +1,5 @@
+declare module 'typed.js' {
+    const Typed: any; // Adjust the type definition as needed
+    export default Typed;
+  }
+  
